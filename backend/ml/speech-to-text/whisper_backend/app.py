d91ae70bdaf7ae499f358536b197d88018174f0b
Christopher Schütz <schuetzchristopher@icloud.com>
2023-04-29 14:47:40 +0200

    update response from transcribe endpoint

--- backend/ml/speech-to-text/whisper_backend/app.py
+++ backend/ml/speech-to-text/whisper_backend/app.py
@@ -7,21 +7,23 @@ app = Flask(__name__)
 
 RECORDINGS_PATH = "tmp_recordings"
 
+
 @app.route('/transcribe', methods=['POST'])
 def transcribe_file():
-  file = request.files['file']
-  if file and file.filename.endswith('.webm'):
-    if not os.path.exists(RECORDINGS_PATH):
-      os.makedirs(RECORDINGS_PATH)
-    file.save(os.path.join(RECORDINGS_PATH, file.filename))
-    
-    # Print transcription:
-    model = whisper.load_model("base")
-    result = model.transcribe(os.path.join(RECORDINGS_PATH, file.filename))
-    print(result["text"])
+    file = request.files['file']
+    if file and file.filename.endswith('.webm'):
+        if not os.path.exists(RECORDINGS_PATH):
+            os.makedirs(RECORDINGS_PATH)
+        file.save(os.path.join(RECORDINGS_PATH, file.filename))
 
-  else:
-    return 'Invalid file format. Please upload a .webm file.'
+        # Print transcription:
+        model = whisper.load_model("base")
+        result = model.transcribe(os.path.join(RECORDINGS_PATH, file.filename))
+        return result["text"]
+
+    else:
+        return 'Invalid file format. Please upload a .webm file.'
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
