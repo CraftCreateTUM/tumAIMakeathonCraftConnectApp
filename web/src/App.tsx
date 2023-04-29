c798fc0a61e00bcfb433d67ae2a4a8efc6610d5b
Miikka Tervo <miikka.tervo@gmail.com>
2023-04-29 19:16:23 +0200

    fix frontend chakra errors

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,15 +14,20 @@ import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
 // Check quick-start docs here: https://www.npmjs.com/package/react-audio-voice-recorder
 
 import "./App.css";
-import { ChakraProvider, extendBaseTheme } from '@chakra-ui/react';
-import chakraTheme from '@chakra-ui/theme'
-import { Button, Heading, ButtonGroup, Center, Stack, IconButton, Box, Textarea} from '@chakra-ui/react'
-import { ChatIcon } from '@chakra-ui/icons'
-import { FaCamera } from 'react-icons/fa';
-import { FaMicrophone } from 'react-icons/fa';
-
-
-
+import { ChakraProvider } from "@chakra-ui/react";
+// import chakraTheme from "@chakra-ui/theme";
+import {
+  Button,
+  Heading,
+  Center,
+  Stack,
+  IconButton,
+  Box,
+  Textarea,
+} from "@chakra-ui/react";
+import { ChatIcon } from "@chakra-ui/icons";
+import { FaCamera } from "react-icons/fa";
+// import { FaMicrophone } from "react-icons/fa";
 
 function App() {
   const [showReportSurvey, setShowReportSurvey] = useState(false);
@@ -103,88 +108,132 @@ function App() {
     setShowTextReportBox(!setShowTextReportBox);
   };
 
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    setTextAreaValue(event.target.value);
+  };
+
   return (
     <ChakraProvider>
       <Box bg="#9AE6B4">
-      <Center>
-      <Stack>
-      <Heading>Craft Connect</Heading>
-      <Button colorScheme='blue' onClick={() => {
-          setShowReportSurvey(!showReportSurvey);
-        }}>
-        Create new report
-      </Button>
-      
-      {showReportSurvey && (
-        <div>
-          <Box borderRadius="md" style={{ height: 300, width: 230, backgroundColor: "#e9e9e9" }}>
-            <div style={{ marginLeft: "3px", marginTop: "6px" }}>
-              <Center>
-              <Heading  as="h3" size="md" style={{ marginBottom: "3px" }}>Status of work </Heading>
-              </Center>
-              <Box as="p" fontSize="xs" fontWeight="bold" color="black">
-                <Heading as="h4" size="md"> Description </Heading>
-              {descriptionSentence.slice(0,100) + "..."}
-              </Box>
-              <Heading as="h4" size="md"> Reason </Heading>
-              <Box as="p" fontSize="xs" fontWeight="bold" color="black">
-              {wholeText.slice(0,100) + "..."}
-              </Box>
-              <Heading as="h4" size="md"> List </Heading>
-              <Box as="p" fontSize="xs" fontWeight="bold" color="black">
-              {bulletList.slice(0,100) + "..."}
-              </Box>
-            </div>
-          </Box>
-          <div>
-          <Center>
-          <IconButton aria-label="Open chat" icon={<ChatIcon />}
-              
+        <Center>
+          <Stack>
+            <Heading>Craft Connect</Heading>
+            <Button
+              colorScheme="blue"
               onClick={() => {
-                setShowTextReportBox(!showTextReportBox);
+                setShowReportSurvey(!showReportSurvey);
               }}
-            />
-            <IconButton aria-label="Open chat" icon={<FaCamera />}
-              
-              onClick={() => {
-               //Insert function here;
-              }}
-            />
-            <IconButton aria-label="Open chat" icon={<FaMicrophone />}
-              
-              onClick={() => {
-               //Insert function here;
-              }}
-            />
-            </Center>
-          </div>
-          {showTextReportBox && (
-            // align on top of each other
-            <Box>
-              <p>What have you done today?</p>
-              <form onSubmit={handleSubmit}>
-                <Textarea
-                  value={textAreaValue}
-                  onChange={(event) => {
-                    setTextAreaValue(event.target.value);
+            >
+              Create new report
+            </Button>
+
+            {showReportSurvey && (
+              <div>
+                <Box
+                  borderRadius="md"
+                  style={{
+                    height: 300,
+                    width: 230,
+                    backgroundColor: "#e9e9e9",
                   }}
-                ></Textarea>
-                <Box>
-                  <button type="submit" className="report-button">
-                    Submit
-                  </button>
+                >
+                  <div style={{ marginLeft: "3px", marginTop: "6px" }}>
+                    <Center>
+                      <Heading
+                        as="h3"
+                        size="md"
+                        style={{ marginBottom: "3px" }}
+                      >
+                        Status of work{" "}
+                      </Heading>
+                    </Center>
+                    <Box as="p" fontSize="xs" fontWeight="bold" color="black">
+                      <Heading as="h4" size="md">
+                        {" "}
+                        Description{" "}
+                      </Heading>
+                      {descriptionSentence.slice(0, 100) + "..."}
+                    </Box>
+                    <Heading as="h4" size="md">
+                      {" "}
+                      Reason{" "}
+                    </Heading>
+                    <Box as="p" fontSize="xs" fontWeight="bold" color="black">
+                      {wholeText.slice(0, 100) + "..."}
+                    </Box>
+                    <Heading as="h4" size="md">
+                      {" "}
+                      List{" "}
+                    </Heading>
+                    <Box as="p" fontSize="xs" fontWeight="bold" color="black">
+                      {bulletList.slice(0, 100) + "..."}
+                    </Box>
+                  </div>
                 </Box>
-              </form>
-            </Box>
-          )}
-        </div>
-      )}
-      </Stack>
-      </Center>
+                <div>
+                  <Center>
+                    <IconButton
+                      aria-label="Open chat"
+                      icon={<ChatIcon />}
+                      onClick={() => {
+                        setShowTextReportBox(!showTextReportBox);
+                      }}
+                    />
+                    <IconButton
+                      aria-label="Open chat"
+                      icon={<FaCamera />}
+                      onClick={() => {
+                        //Insert function here;
+                      }}
+                    />
+                    <button
+                      className="transcribe-button"
+                      onClick={transcribeAudio}
+                    >
+                      Transcribe audio
+                    </button>
+
+                    <div>
+                      <AudioRecorder
+                        onRecordingComplete={(blob: Blob) =>
+                          addAudioElement(blob)
+                        }
+                        recorderControls={recorderControls}
+                      />
+                      <br />
+                      <button onClick={recorderControls.stopRecording}>
+                        Stop recording
+                      </button>
+                      <br />
+                    </div>
+                  </Center>
+                </div>
+                {showTextReportBox && (
+                  // align on top of each other
+                  <Box>
+                    <p>What have you done today?</p>
+                    <form onSubmit={handleSubmit}>
+                      <Textarea
+                        value={textAreaValue}
+                        onChange={handleTextAreaChange}
+                      ></Textarea>
+                      <Box>
+                        <button type="submit" className="report-button">
+                          Submit
+                        </button>
+                      </Box>
+                    </form>
+                  </Box>
+                )}
+              </div>
+            )}
+          </Stack>
+        </Center>
       </Box>
     </ChakraProvider>
   );
-  
 }
 
 export default App;
