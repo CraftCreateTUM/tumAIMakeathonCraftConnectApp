6771d7dc6a10f32e3b4447284100f02ca0d0d1f9
Marc Brede <marc.brede@tum.de>
2023-04-29 13:14:50 +0200

    add endpoint

--- backend/gpt_service/server.py
+++ backend/gpt_service/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from gpt import get_bulletlist, get_description_sentence
+from flask import send_file
+from pdf import create_pdf
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +20,14 @@ def chat_gpt():
         return jsonify({'message': get_description_sentence(request.json['text'])})
     except Exception as e:
         return jsonify({'error': str(e)})
+
+@app.route('/pdf', methods=['POST'])
+def return_files_tut():
+    path = create_pdf(request.get_json())
+    try:
+        return send_file(path, attachment_filename='report.pdf')
+    except Exception as e:
+        return str(e)
     
 if __name__ == '__main__':
     app.run(debug=True)
